ba4557d9b0cb2d338a8900c90944fe4e1c82b4ff
Zahra Masoumi <zahraamasoumi@gmail.com>
2024-02-08 13:49:58 +0330

    fix: just fix some bugs..

--- ReporterSolution/TestReporter/Program.cs
+++ ReporterSolution/TestReporter/Program.cs
@@ -1,51 +1,46 @@
 ﻿using System.Reflection;
+using Task = ImplementationBase.models.Task;
+
 
 Console.WriteLine("Hello, World!");
 
 var dllFile = new FileInfo(@".\plugins\Reporter.dll");
 var reporterDll = Assembly.LoadFile(dllFile.FullName);
 
-dllFile = new FileInfo(@".\plugins\Data.dll");
+dllFile = new FileInfo(@".\plugins\DataProviderOne.dll");
 var dataDll = Assembly.LoadFile(dllFile.FullName);
 
 
 var type = dataDll.GetTypes().Where(t => t.GetInterface("IDataProvider") != null).First();
-var tasktype = dataDll.GetTypes().Where(t => t.Name == "Task").First();
+//var tasktype = dataDll.GetTypes().Where(t => t.Name == "Task").First();
 var mi = type.GetMethod("GetData");
 
-List<type> res = mi.Invoke(Activator.CreateInstance(type), null);
-
-Type listType = typeof(List<>).MakeGenericType(taskType);
-object list = Activator.CreateInstance(listType);
+var res = (List<Task>)mi.Invoke(Activator.CreateInstance(type), null);
 
 
-var processor = reporterDll.GetType("TaskProcessor");
-var queries = processor.GetMethods();
+var processor = reporterDll.GetType("Reporter.TaskProcessor");
+var queryMethods = processor.GetMethods();
 
 string q = "Count";
 
-if (q ==  "Count" && res != null)
+if (q == "Count" && res != null)
 {
-    var resQ = queries.Where(q => q.Name == "Count").First().Invoke(null, res);
+    object[] arg = { res };
+    var resQ = queryMethods.Where(q => q.Name == "CountTasks").First().Invoke(null, arg);
 }
 
 int n = 20;
 
 if (n > 10)
 {
-    int m = n%10;
+    int m = n % 10;
     for (int i = 0; i < m; i++)
     {
-        List<int> list = new List<int> ();
+        List<int> list = new List<int>();
         for (int j = 0; j < 10; j++)
         {
-            list.Add (j);
+            list.Add(j);
         }
-
-        Task.Run(() =>
-        {
-            Console.WriteLine("hii");
-        });
     }
 }
 

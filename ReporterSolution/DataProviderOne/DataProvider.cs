d4f231d19fe9b06a1e93d4046ce3b020d3c22cbd
Zahra Masoumi <zahraamasoumi@gmail.com>
2024-02-09 17:23:41 +0330

    feat: add new data provider: read from json file

---
+++ ReporterSolution/DataProviderOne/DataProvider.cs
@@ -0,0 +1,25 @@
+﻿using DataProviderOne.DataReader;
+using ImplementationBase.interfaces;
+using Task = ImplementationBase.models.Task;
+
+namespace DataProviderOne;
+
+public class DataProvider : IDataProvider
+{
+    public IEnumerable<Task> GetData()
+    {
+        return ReadFile("tasks.json");
+    }
+
+    List<Task> ReadFile(string path)
+    {
+        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        var absolutePath = Path.Combine(baseDirectory, path);
+
+        IFileReader<Task> reader = new JsonFileReader<Task>();
+
+        List<Task> tasks = reader.ReadFile(absolutePath);
+
+        return tasks;
+    }
+}
---
+++ ReporterSolution/DataProviderOne/DataReader/IFileReader.cs
@@ -0,0 +1,6 @@
+﻿namespace DataProviderOne.DataReader;
+
+public interface IFileReader<T>
+{
+    List<T> ReadFile(string filePath);
+}
